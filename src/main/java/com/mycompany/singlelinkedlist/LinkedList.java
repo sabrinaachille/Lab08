ee08f0c24e6272249ae537781d45bffcbd9c3f53
Moaath Alrajab <moaath@alrajab.net>
2021-10-18 11:13:19 -0400

    Update no search and print

--- src/main/java/com/mycompany/singlelinkedlist/LinkedList.java
+++ src/main/java/com/mycompany/singlelinkedlist/LinkedList.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.singlelinkedlist;
 
 class Node {
@@ -45,14 +42,7 @@ class LinkedList {
       }
    }
    
-   public void printList() {
-      Node node = head;
-      while (node != null) {
-         System.out.print(node.data + " ");
-         node = node.next;
-      }
-      System.out.println();
-   }
+
    
    public void insertAfter(Node currentNode, Node newNode) {
       if (head == null) {
